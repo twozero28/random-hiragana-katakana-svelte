3f2540ebd9075d7c79208f42ab3735a4ad4019d7
twozero28 <28twozero@gmail.com>
2024-07-26 06:49:04 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import NavBar from "./lib/components/NavBar.svelte";
+
+</script>
+
+<header>
+  <NavBar/>
+</header>
+
+<main>
+  
+</main>
+
+<style>
+  header {
+    width: 100vw;
+    position: fixed;
+  }
+  main {
+    height: 100vh;
+  }
+</style>
---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,18 @@
+<script>
+    import GithubIcon from "./GithubIcon.svelte";
+
+  
+</script>
+
+<nav class="bar">
+	<div></div>
+	<GithubIcon />
+</nav>
+
+<style>
+  .bar {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+</style>
